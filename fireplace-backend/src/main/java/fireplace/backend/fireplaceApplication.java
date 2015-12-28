1a8f0c298d2f9e3620e138b946e89f5b1906c915
timmygilissen <timmy.gilissen@gmail.com>
2015-12-28 21:56:22 +0100

    temp

--- fireplace-backend/src/main/java/fireplace/backend/fireplaceApplication.java
+++ fireplace-backend/src/main/java/fireplace/backend/fireplaceApplication.java
@@ -6,12 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 @SpringBootApplication
 public class fireplaceApplication {
     public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("rabbitmqConfig.xml");
 
         SpringApplication.run(fireplaceApplication.class, args);
 
